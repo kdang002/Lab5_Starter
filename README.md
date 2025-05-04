f92fdc5ad1c3f7952970f6562934fa10e534af48
kiet dang <kid002@ucsd.edu>
2025-05-03 17:18:56 -0700

    finished expose - party horn. Need to publish expose.html on GitHub pages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 5 - Starter
+## [SELF NOTE] REMEMBER TO REMOVE ExecutionPolicy on PowerShell when done with this lab:
+```Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope CurrentUser ```
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,106 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+
+
+function init() 
+{
+  //Get <select> with id = horn-select
+  const hornSelectElm = document.getElementById("horn-select");
+
+  //Returns the first element in document with id = expose, tag = img
+  const img = document.querySelector("#expose img"); 
+  
+  //Returns the first element in document with id = expose, tag = audio
+  const audio =  document.querySelector("#expose audio");
+
+  //HornSelector Event: Changes image and audio source
+  hornSelectElm.addEventListener('change', function ()
+  {
+    const horn = hornSelectElm.value;
+    let imgPath = "";
+    let audioPath = "";
+
+    switch(horn)
+    {
+      case 'air-horn':
+        imgPath = "assets/images/air-horn.svg";
+        audioPath = "assets/audio/air-horn.mp3";
+        break;
+
+      case 'car-horn':
+        imgPath = "assets/images/car-horn.svg";
+        audioPath = "assets/audio/car-horn.mp3";
+        break;
+
+      case 'party-horn':
+        imgPath = "assets/images/party-horn.svg";
+        audioPath = "assets/audio/party-horn.mp3";
+        break;
+
+      default:
+        imgPath = "assets/images/no-image.svg"
+        audioPath = "";
+        break;
+    }
+
+    img.src = imgPath;
+    audio.src = audioPath;
+  });
+
+  //Get "Volume Control" slider element
+  const volCtrlElm = document.querySelector("#volume-controls input");
+  const volCtrlImg = document.querySelector("#volume-controls img");
+  //playButtonElm Event: When "Play Sound" button is click, play audio
+  volCtrlElm.addEventListener("input", function() 
+  {
+    let volValue = Number(volCtrlElm.value);
+    audio.volume = volValue / 100;
+
+    if(volValue <= 0)
+    {
+      volCtrlImg.src = "assets/icons/volume-level-0.svg";
+    }
+
+    else if(volValue > 0 && volValue < 33)
+    {
+      volCtrlImg.src = "assets/icons/volume-level-1.svg";
+    }
+
+    else if(volValue >= 33  && volValue < 67)
+    {
+      volCtrlImg.src = "assets/icons/volume-level-2.svg";
+    }
+
+    else if(volValue >= 67)
+    {
+      volCtrlImg.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+  
+
+  const jsConfetti = new JSConfetti();
+  
+  jsConfetti.addConfetti({
+    emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+ })
+ 
+  //Get "Play Sound" button element
+  const playButtonElm = document.querySelector("#expose button");
+  //playButtonElm Event: When "Play Sound" button is click, play audio
+  playButtonElm.addEventListener("click", function() 
+  {
+    if(audio.src)
+      {
+        audio.load();
+        audio.play();
+      }
+
+      //Party horn: Add confetti
+      if (hornSelectElm.value == "party-horn")
+        {
+          jsConfetti.addConfetti();
+        } 
+  });
 }
